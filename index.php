b77aca8a6233475265f6ba796746e3b2ee61ad95
Mribrahimkhan360 <mribrahimkhan360@gmail.com>
2025-03-24 09:29:26 +0600

    Browser supporting code

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<script>
+		var xmlhttp;
+		if (window.XMLHttpRequest) {
+		    // For modern browsers
+		    xmlhttp = new XMLHttpRequest();
+		} else {
+		    // For older versions of Internet Explorer (IE6, IE5)
+		    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+
+	</script>>
+</body>
+</html>
